ecb311f4104ed7f6121b13eae38aac854ff55aaa
Gustavo Okamoto <gustavookamoto1945@gmail.com>
2022-10-05 21:15:01 -0300

    fazendo alterações e atualizando em 05/10

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function addProduto(){
     produto = document.getElementById('name').value
     codigo = document.getElementById('number').value
     preco = document.getElementById('preco').value
+    let li = document.querySelectorAll('li')
+    let resposta2 = true
+
 
     //objeto criado e instânciado
     produtoF = {"marca": marca, "produto": produto, "codigo": codigo, "preco": preco}
@@ -20,47 +23,77 @@ function addProduto(){
     var respostaP = confirm("Deseja adicionar produto?") //Botão de confirmar
 
     if (respostaP == true){  //caso esteja confirmado irá funcionar
-        // Verificação de marca e separação
-    if(produtoF.marca == "Seara"){ 
-        //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Seara
-        let elemento = document.createElement('li')
-        elemento.className = `${produtoF.codigo}`
-        //cria um novo elemento no HTML
-        document.querySelector('#lista-seara').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
-    
-    } else if (produtoF.marca == "Swift"){
-        //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Swift
-        let elemento = document.createElement('li')
-        elemento.className = `${produtoF.codigo}`
-        //cria um novo elemento no HTML
-        document.querySelector('#lista-swift').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
-    } else {
-        //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Friboi
-        let elemento = document.createElement('li')
-        elemento.className = `${produtoF.codigo}`
-        //cria um novo elemento no HTML
-        document.querySelector('#lista-friboi').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
-    }
-    }
+        // Verifica se o código já foi utilizado
+        for (i = 0; i < li.length; i++){
+            if (li[i].classList == produtoF.codigo){
+                //produto com código igual
+                alert("Código de produto já utilizado!")
+                resposta2 = false
+            } else {
+                resposta2 = true
+            }
+        }
+
+        if (resposta2 == true){
+            // Verificação de marca e separação
+            if(produtoF.marca == "Seara"){ 
+                //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Seara
+                let elemento = document.createElement('li')
+                elemento.classList.add(`${produtoF.codigo}`)
+                //cria um novo elemento no HTML
+                document.querySelector('#lista-seara').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
+            
+            } else if (produtoF.marca == "Swift"){
+                //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Swift
+                let elemento = document.createElement('li')
+                elemento.classList.add(`${produtoF.codigo}`)
+                //cria um novo elemento no HTML
+                document.querySelector('#lista-swift').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
+            } else {
+                //Cria um novo elemento (li) com a classe igual ao código do produto --> dos produtos da marca Friboi
+                let elemento = document.createElement('li')
+                elemento.classList.add(`${produtoF.codigo}`)
+                //cria um novo elemento no HTML
+                document.querySelector('#lista-friboi').appendChild(elemento).innerHTML = `(${produtoF.codigo}) - Marca: ${produtoF.marca} - ${produtoF.produto} R$ ${produtoF.preco}`
+            }
+        }
+        }
+
+    // Apagando os valores dos inputs
+    document.getElementById('name').value = ""
+    document.getElementById('number').value = ""
+    document.getElementById('preco').value = ""
+    document.getElementById('marca').value = ""
  
 }
 
 //Função para remover as informações do produto
 function removeProduto(){
     //Captura a lista de li's existentes
-    var li = document.querySelectorAll('li')
+    let li = document.querySelectorAll('li')
+
     //captura o código do produto que o usuário deseja apagar
     var resposta = document.querySelector('#codigo_remover').value
-    var respostaD = confirm("Deseja adicionar produto?") //Botão de confirmar
+    var respostaD = confirm("Deseja excluir produto?") //Botão de confirmar
+    var verificacao2 = 0;
 
     if (respostaD == true){ //caso esteja confirmado irá funcionar
-        //Loop para verificar todos os li's com o código do produto que deseja apagar
+        //verificação todos os li's com o código do produto que deseja apagar
+
         for (i = 0; i < li.length; i++){
-            if (li[i].className == resposta){
+            if (li[i].classList == resposta){
                 //Substitui o conteudo HTML
-                li[i].innerHTML = ""
+                li[i].remove()
+            } else {
+                verificacao2++
             }
-    }
+        }
+
+        // Se o valor bater, ele aparece a mensagem
+        if (verificacao2 == li.length){
+            alert('O código não existe')
+        }
     }
 
+    document.getElementById('codigo_remover').value = ""
 }
